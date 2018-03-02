5102505e1a64c0c1f4760c7732524dc0f5858e44
otabut <33980031+otabut@users.noreply.github.com>
2018-03-02 13:16:10 +0100

    Update New-PXFeature.ps1

--- module/functions/New-PXFeature.ps1
+++ module/functions/New-PXFeature.ps1
@@ -2,7 +2,9 @@ Function New-PXFeature
 {
   param (
     [Parameter(Mandatory=$true)]$Name,
-    [Parameter(Mandatory=$false)][ValidateSet("yes","no")]$Active='no'
+    [Parameter(Mandatory=$false)][ValidateSet("yes","no")]$Active='no',
+    [Parameter(Mandatory=$false)][switch]$CreateFile,
+    [Parameter(Mandatory=$false)][ValidatePattern("v\d*")]$APIversion='v1'
   )
 
   $ErrorActionPreference = "stop"
@@ -23,6 +25,15 @@ Function New-PXFeature
       }
       $Config.features += $Feature
       $Config | ConvertTo-Json -Depth 5 | Set-Content $ConfigurationFile
+
+      if ($CreateFile)
+      {
+        if (!(test-path "${ENV:ProgramFiles}\PowerXaaS\api\$APIversion"))
+        {
+          New-Item "${ENV:ProgramFiles}\PowerXaaS\api\$APIversion" -ItemType Directory | Out-Null
+        }
+        Copy-Item $PSScriptRoot\feature-template.txt "${ENV:ProgramFiles}\PowerXaaS\api\$APIversion\$Name.ps1" | Out-Null
+      }
     }
   }
   catch
