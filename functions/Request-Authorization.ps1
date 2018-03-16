571c8ac632e7118871e919e17091d5be44ab10ef
otabut <33980031+otabut@users.noreply.github.com>
2018-03-16 10:11:28 +0100

    Update Request-Authorization.ps1

--- functions/Request-Authorization.ps1
+++ functions/Request-Authorization.ps1
@@ -9,6 +9,8 @@ Function Request-Authorization
 
   try
   {
+    . "${ENV:ProgramFiles}\PowerXaaS\Functions\JWT-helper.ps1"
+
     $Claims = (Decode-JWT $Token).Claims
     if ([datetime]$Claims."expiration-date" -gt (Get-Date))
     {
@@ -31,37 +33,4 @@ Function Request-Authorization
   {
     return "NotAuthenticated"
   }
-}
-
-
-function Convert-FromBase64StringWithNoPadding([string]$data)
-{
-    $data = $data.Replace('-', '+').Replace('_', '/')
-    switch ($data.Length % 4)
-    {
-        0 { break }
-        2 { $data += '==' }
-        3 { $data += '=' }
-        default { throw New-Object ArgumentException('data') }
-    }
-    return [System.Convert]::FromBase64String($data)
-
-}
-
-
-function Decode-JWT([string]$rawToken)
-{
-    $parts = $rawToken.Split('.');
-    $headers = [System.Text.Encoding]::UTF8.GetString((Convert-FromBase64StringWithNoPadding $parts[0]))
-    $claims = [System.Text.Encoding]::UTF8.GetString((Convert-FromBase64StringWithNoPadding $parts[1]))
-    $signature = (Convert-FromBase64StringWithNoPadding $parts[2])
-
-    $customObject = [PSCustomObject]@{
-        headers = ($headers | ConvertFrom-Json)
-        claims = ($claims | ConvertFrom-Json)
-        signature = $signature
-    }
-
-    Write-Verbose -Message ("JWT`r`n.headers: {0}`r`n.claims: {1}`r`n.signature: {2}`r`n" -f $headers,$claims,[System.BitConverter]::ToString($signature))
-    return $customObject
 }
