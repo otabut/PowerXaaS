a614dfe7f7f2047ea3f184be93a55d15a6d516ad
otabut <33980031+otabut@users.noreply.github.com>
2018-03-25 17:03:03 +0200

    Update PowerXaaS.tests.ps1

--- pester/PowerXaaS.tests.ps1
+++ pester/PowerXaaS.tests.ps1
@@ -41,7 +41,7 @@ Describe "Validate PowerXaaS" {
     It "Version" {
 
       $result = & "$PSScriptRoot\..\PowerXaaS.ps1" -Version
-      $result | should be "1.4.0"
+      $result | should be "1.5.0"
     }
 
     It "Start" {
